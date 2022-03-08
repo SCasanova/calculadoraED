65adda8c6263fb3d5d89fdfbaee8156bb66b07e8
Leo11wasd <98835751+Leo11wasd@users.noreply.github.com>
2022-03-07 19:03:30 -0600

    Update CalculadoraED.java

--- CalculadoraED.java
+++ CalculadoraED.java
@@ -18,184 +18,6 @@ public class CalculadoraED extends javax.swing.JFrame {
     boolean bandera2 = false;
 
     /**
-     * Regresa el número de elementos de una pila dada.
-     *
-     * @param pila Pila de la cual se quiere conocer su número de elementos.
-     * @return - int: El número de elementos en la pila.
-     */
-    public static <T> int numEle(PilaADT<T> pila) {
-        int contador = 0;
-        ArrayList<T> apoyo = new ArrayList();
-        while (!pila.isEmpty()) {
-            apoyo.add(pila.pop());
-            contador++;
-        }
-        int i = apoyo.size() - 1;
-        while (i >= 0) { //lo regresa a su pos original pa q la pila no se vea afectada
-            pila.push(apoyo.get(i));
-            i--;
-        }
-        return contador;
-    }
-
-    /**
-     * Determina si un carácter dado es un número o un punto decimal.
-     *
-     * @param car Caracter a evaluar.
-     * @return <ul>
-     * <li>true: si el carácter es un número o un punto decimal.
-     * <li>false: si el carácter no es un número o punto decimal.
-     * </ul>
-     */
-    public static boolean esNum(Character car) {
-        boolean res = false;
-        if (car == '0' || car == '1' || car == '2' || car == '3' || car == '4' || car == '5' || car == '6' || car == '7' || car == '8' || car == '9' || car == '.') {
-            res = true;
-        }
-        return res;
-    }
-
-    /**
-     * Determina si un carácter dado es un operador (excepto -).
-     *
-     * @param car Caracter a evaluar.
-     * @return <ul>
-     * <li>true: si el carácter es un operador.
-     * <li>false: si el carácter no es un operador.
-     * </ul>
-     */
-    public static boolean esSigno(Character car) { //no incluye el -
-        boolean res = false;
-        if (car == '+' || car == '*' || car == '/' || car == '^') {
-            res = true;
-        }
-        return res;
-    }
-
-    /**
-     * Determina si una expresión matemática tiene una correcta sintaxis.
-     *
-     * @param cad Expresión a evaluar.
-     * @return <ul>
-     * <li>true: si la expresión tiene sintaxis correcta.
-     * <li>false: si la expresión tiene sintaxis incorrecta.
-     * </ul>
-     */
-    public static boolean malSignos(String cad) {
-        boolean res = false;
-        PilaA<Character> p = new PilaA();
-        int i = 0;
-        if (!esSigno(cad.charAt(cad.length() - 1)) && cad.charAt(cad.length() - 1) != '-') { //si el ultimo elemento no es signo
-            while (i < cad.length()) {
-                if (esSigno(cad.charAt(i))) { //es signo (excepto el menos
-                    try {
-                        p.pop();
-                    } catch (Exception e) {
-                        break;
-                    }
-                } else if (cad.charAt(i) == '-') {
-                    if (i != 0 && cad.charAt(i - 1) != '(') {
-                        if (cad.charAt(i + 1) == ')') { //(3-)
-                            break;
-                        }
-                        if (esSigno(cad.charAt(i - 1)) || cad.charAt(i - 1) == '-') {// 3*-2 o 3--2
-                            break;
-                        } else if (esSigno(cad.charAt(+1)) || cad.charAt(i + 1) == '-') {// 3-*2   o 3--2
-                            break;
-                        } else {
-                            if (esNum(cad.charAt(i - 1))) {//funge como operador
-                                try {
-                                    p.pop();
-                                } catch (Exception e) {
-                                    break;
-                                }
-                            }
-                        }
-
-                    }
-
-                } else if (cad.charAt(i) == '(') {
-                    if (!p.isEmpty()) {//la expersion esta bien antes de '(' si la pila esta vacía
-                        break;
-
-                    }
-                } else if (cad.charAt(i) == ')') {
-                    if (numEle(p) != 1) {//la expersion esta bien al salir del parentesis si la pila tiene 1 elemento
-                        break;
-                    }
-                } else { //si es num, punto decimal
-                    if (i == 0) { //push el numero o decimal en la pos 0
-                        p.push(cad.charAt(i));
-                    } else if (esSigno(cad.charAt(i - 1)) || cad.charAt(i - 1) == ')' || cad.charAt(i - 1) == '(' || cad.charAt(i - 1) == '-') {  //pa q sirva con nums de varios digitos
-                        p.push(cad.charAt(i));
-                    }
-                }
-                i++;
-            }
-
-        }
-        if (i == cad.length() && numEle(p) == 1) {
-            res = true;
-        }
-
-        return res;
-    }
-
-    /**
-     * Determina si una cadena cuenta con una cantidad y acomodo adecuado de
-     * paréntesis.
-     *
-     * @param cad Cadena a evaluar
-     * @return <ul>
-     * <li>true: si la expresión tiene paréntesis adecuados.
-     * <li>false: si la expresión tiene no paréntesis adecuados.
-     * </ul>
-     */
-    public static boolean parentesisBal(String cad) {
-        PilaA<Character> p = new PilaA();
-        int i = 0;
-        boolean res = false;
-        while (i < cad.length()) {
-            if (cad.charAt(i) == '(') {
-                p.push(cad.charAt(i)); //me lo puedo ahorrar y decirle push('A') o cualquier caracter
-            }
-            if (cad.charAt(i) == ')') {
-                try {
-                    p.pop(); //lanza la exception hecha en clase 
-                } catch (Exception e) { //si lanza exception, pa q no me pare el programa y c vea feo
-                    break;
-                }
-            }
-            i++;
-        }
-        if (i == cad.length() && p.isEmpty()) {
-            res = true;
-        }
-
-        return res;
-    }
-
-    /**
-     * Determina si una si una expresión matemática cuenta con una correcta
-     * sintaxis y un uso adecuado de paréntesis.
-     *
-     * @param cad Cadena a evaluar
-     * @return <ul>
-     * <li>true: si la expresión cumple con ambas condiciones.
-     * <li>false: si la expresión no cumple al menos una de las dos condiciones.
-     * </ul>
-     */
-    public static boolean estaBien(String cad) {
-        boolean res = false;
-        Integer n = 0;
-
-        if (malSignos(cad) && parentesisBal(cad)) {
-            res = true;
-        }
-        return res;
-    }
-
-    /**
      * Creates new form CalculadoraED
      */
     public CalculadoraED() {
@@ -521,7 +343,7 @@ public class CalculadoraED extends javax.swing.JFrame {
             jTextField_escribeOperacion.setText("");
             bandera = false;
         }
-        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "7");        // TODO add your handling code here:
+        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "7");     
     }//GEN-LAST:event_jButton_sieteActionPerformed
 
     private void jButton_seisActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_seisActionPerformed
@@ -529,7 +351,7 @@ public class CalculadoraED extends javax.swing.JFrame {
             jTextField_escribeOperacion.setText("");
             bandera = false;
         }
-        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "6");        // TODO add your handling code here:
+        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "6");       
     }//GEN-LAST:event_jButton_seisActionPerformed
 
     private void jButton_ochoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_ochoActionPerformed
@@ -537,7 +359,7 @@ public class CalculadoraED extends javax.swing.JFrame {
             jTextField_escribeOperacion.setText("");
             bandera = false;
         }
-        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "8");        // TODO add your handling code here:
+        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "8");       
     }//GEN-LAST:event_jButton_ochoActionPerformed
 
     private void jButton_cuatroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_cuatroActionPerformed
@@ -559,7 +381,7 @@ public class CalculadoraED extends javax.swing.JFrame {
             jTextField_escribeOperacion.setText("");
             bandera = false;
         }
-        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "2");        // TODO add your handling code here:
+        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "2");      
     }//GEN-LAST:event_jButton_dosActionPerformed
 
     private void jButton_unoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_unoActionPerformed
@@ -567,7 +389,7 @@ public class CalculadoraED extends javax.swing.JFrame {
             jTextField_escribeOperacion.setText("");
             bandera = false;
         }
-        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "1");        // TODO add your handling code here:
+        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "1");       
     }//GEN-LAST:event_jButton_unoActionPerformed
 
     private void jButton_ceroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_ceroActionPerformed
@@ -575,7 +397,7 @@ public class CalculadoraED extends javax.swing.JFrame {
             jTextField_escribeOperacion.setText("");
             bandera = false;
         }
-        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "0");        // TODO add your handling code here:
+        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "0");        
     }//GEN-LAST:event_jButton_ceroActionPerformed
 
     private void jButton_puntoDecimalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_puntoDecimalActionPerformed
@@ -610,7 +432,7 @@ public class CalculadoraED extends javax.swing.JFrame {
             jTextField_escribeOperacion.setText("");
             bandera = false;
         }bandera2 = true;
-        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "-");        // TODO add your handling code here:
+        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "-");        
     }//GEN-LAST:event_jButton_signoMenosActionPerformed
 
     private void jButton_signoMasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_signoMasActionPerformed
@@ -618,7 +440,7 @@ public class CalculadoraED extends javax.swing.JFrame {
             jTextField_escribeOperacion.setText("");
             bandera = false;
         }bandera2 = true;
-        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "+");        // TODO add your handling code here:
+        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "+");        
     }//GEN-LAST:event_jButton_signoMasActionPerformed
 
     private void jButton_signoIgualActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_signoIgualActionPerformed
@@ -658,7 +480,7 @@ public class CalculadoraED extends javax.swing.JFrame {
             jTextField_escribeOperacion.setText("");
             bandera = false;
         }
-        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "(");        // TODO add your handling code here:
+        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "(");       
     }//GEN-LAST:event_jButton_parAbreActionPerformed
 
     private void jButton_parCierraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_parCierraActionPerformed
@@ -666,7 +488,7 @@ public class CalculadoraED extends javax.swing.JFrame {
             jTextField_escribeOperacion.setText("");
             bandera = false;
         }
-        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + ")");        // TODO add your handling code here:
+        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + ")");        
 
     }//GEN-LAST:event_jButton_parCierraActionPerformed
 
@@ -675,7 +497,7 @@ public class CalculadoraED extends javax.swing.JFrame {
             jTextField_escribeOperacion.setText("");
             bandera = false;
         }
-        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "5");        // TODO add your handling code here:
+        jTextField_escribeOperacion.setText(jTextField_escribeOperacion.getText() + "5");      
     }//GEN-LAST:event_jButton_cinco1ActionPerformed
 
     /**
